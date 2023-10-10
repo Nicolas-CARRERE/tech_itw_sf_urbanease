722f68ead74ad800bf55c72a06dab6ab00e34c72
Brune <abrune.osteo@gmail.com>
2023-10-10 16:03:59 +0200

    Implement boat movement and map updates with treasures
    Here is a summary of what was implemented:
    - Loaded data into the database using fixtures.
    - Added boat movement functionality to the BoatController and check if boat foudn a treasure.
    - Created a MapManager service for checking tile existence get random island and place and delete treasures.
    - Displayed error messages for invalid moves and prevent the boat from moving.
    - Added treasure functionality to tiles.
    - Implemented a 'start' method to reset the game in MapController.
    - Checked for treasure after each boat movement.
    
    The code now supports boat navigation, tile existence checks, and treasure detection.
    
    BRUNE_ADRIEN

--- src/Controller/BoatController.php
+++ src/Controller/BoatController.php
@@ -2,107 +2,106 @@
 
 namespace App\Controller;
 
+use App\Entity\Tile;
 use App\Entity\Boat;
 use App\Form\BoatType;
 use App\Repository\BoatRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Services\MapManager;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 /**
  * @Route("/boat")
  */
 class BoatController extends AbstractController
 {
+    private $mapManager;
+    private $session;
+
+    public function __construct(MapManager $mapManager, SessionInterface $session)
+    {
+        $this->mapManager = $mapManager;
+        $this->session = $session;
+    }
 
     /**
-     * Move the boat to coord x,y
-     * @Route("/move/{x}/{y}", name="moveBoat", requirements={"x"="\d+", "y"="\d+"}))
+     * Move the boat in a specified direction (N, S, E, W)
+     * @Route("/direction/{direction}", name="moveDirection", requirements={"direction"="N|S|E|W"})
      */
-    public function moveBoat(int $x, int $y, BoatRepository $boatRepository, EntityManagerInterface $em) :Response
-    {
-        $boat = $boatRepository->findOneBy([]);
-        $boat->setCoordX($x);
-        $boat->setCoordY($y);
+    public function moveDirection(
+        string $direction,
+        BoatRepository $boatRepository,
+        EntityManagerInterface $em,
+        LoggerInterface $logger
+    ): Response {
+        // Log a debug message to indicate that the moveDirection method is called
+        $logger->debug('Moving the boat in direction: ' . $direction);
 
+        // Get the current boat entity from the repository
+        $boat = $boatRepository->findOneBy([]);
+
+        // Get the current X and Y coordinates of the boat
+        $newX = $boat->getCoordX();
+        $newY = $boat->getCoordY();
+
+        // Update the X and Y coordinates based on the specified direction
+        switch ($direction) {
+            case 'N':
+                $newY--;
+                break;
+            case 'S':
+                $newY++;
+                break;
+            case 'E':
+                $newX++;
+                break;
+            case 'W':
+                $newX--;
+                break;
+            default:
+                break;
+        }
+
+        // Check if the new tile exists on the map
+        if (!$this->mapManager->tileExists($newX, $newY)) {
+            // Set a flash message for attempting to move off the map
+            $this->session->getFlashBag()->add('error', 'Cannot go off the map.');
+
+            // Redirect back to the map without updating the boat's coordinates
+            return $this->redirectToRoute('map');
+        }
+
+        // Update the boat's coordinates
+        $boat->setCoordX($newX);
+        $boat->setCoordY($newY);
+
+        // Save the updated boat coordinates to the database
         $em->flush();
 
+        // Check if the boat has found the treasure
+        if ($this->mapManager->checkTreasure($boat)) {
+            // Display a success flash message
+            $this->addFlash('success', 'Congratulations! You found the treasure.');
+        }
+
+        // Redirect back to the map
         return $this->redirectToRoute('map');
     }
 
-
     /**
-     * @Route("/", name="boat_index", methods="GET")
+     * @Route("/test-move-direction/{direction}", name="test_move_direction", requirements={"direction"="N|S|E|W"})
      */
-    public function index(BoatRepository $boatRepository): Response
+    public function testMoveDirection(string $direction, BoatRepository $boatRepository, EntityManagerInterface $em, LoggerInterface $logger): Response
     {
-        return $this->render('boat/index.html.twig', ['boats' => $boatRepository->findAll()]);
-    }
-
-    /**
-     * @Route("/new", name="boat_new", methods="GET|POST")
-     */
-    public function new(Request $request): Response
-    {
-        $boat = new Boat();
-        $form = $this->createForm(BoatType::class, $boat);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($boat);
-            $em->flush();
-
-            return $this->redirectToRoute('boat_index');
-        }
-
-        return $this->render('boat/new.html.twig', [
-            'boat' => $boat,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="boat_show", methods="GET")
-     */
-    public function show(Boat $boat): Response
-    {
-        return $this->render('boat/show.html.twig', ['boat' => $boat]);
-    }
-
-    /**
-     * @Route("/{id}/edit", name="boat_edit", methods="GET|POST")
-     */
-    public function edit(Request $request, Boat $boat): Response
-    {
-        $form = $this->createForm(BoatType::class, $boat);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('boat_index', ['id' => $boat->getId()]);
-        }
-
-        return $this->render('boat/edit.html.twig', [
-            'boat' => $boat,
-            'form' => $form->createView(),
-        ]);
-    }
-
-    /**
-     * @Route("/{id}", name="boat_delete", methods="DELETE")
-     */
-    public function delete(Request $request, Boat $boat): Response
-    {
-        if ($this->isCsrfTokenValid('delete' . $boat->getId(), $request->request->get('_token'))) {
-            $em = $this->getDoctrine()->getManager();
-            $em->remove($boat);
-            $em->flush();
-        }
-
-        return $this->redirectToRoute('boat_index');
+        // Log a debug message to indicate that the testMoveDirection method is called
+        $logger->debug('Testing boat movement in direction: ' . $direction);
+    
+        // Call the moveDirection method to simulate the boat's movement
+        return $this->moveDirection($direction, $boatRepository, $em, $logger);
     }
 }
--- src/Controller/MapController.php
+++ src/Controller/MapController.php
@@ -3,30 +3,69 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Tile;
+use App\Entity\Boat; // Import the Boat entity
 use App\Repository\BoatRepository;
+use App\Services\MapManager;
+use Doctrine\ORM\EntityManagerInterface;
 
 class MapController extends AbstractController
 {
     /**
+     * Display the map and boat's current position.
+     *
      * @Route("/map", name="map")
      */
-    public function displayMap(BoatRepository $boatRepository) :Response
+    public function displayMap(BoatRepository $boatRepository): Response
     {
+        // Get the Entity Manager
         $em = $this->getDoctrine()->getManager();
+
+        // Retrieve all tiles from the database
         $tiles = $em->getRepository(Tile::class)->findAll();
 
+        // Create a map structure using tile coordinates as keys
         foreach ($tiles as $tile) {
             $map[$tile->getCoordX()][$tile->getCoordY()] = $tile;
         }
 
+        // Retrieve the current boat entity
         $boat = $boatRepository->findOneBy([]);
 
+        // Render the map view with map data and boat information
         return $this->render('map/index.html.twig', [
-            'map'  => $map ?? [],
-            'boat' => $boat,
+            'map'  => $map ?? [], // Pass the map data to the view (or an empty array if none)
+            'boat' => $boat,     // Pass the boat entity to the view
         ]);
+    }
+
+    /**
+     * Start a new game by resetting the boat's coordinates and placing the treasure on a random island.
+     *
+     * @Route("/start", name="start_game")
+     */
+    public function start(MapManager $mapManager, EntityManagerInterface $entityManager): RedirectResponse
+    {
+        // Reset the boat's coordinates to (0, 0)
+        $boat = $this->getDoctrine()->getRepository(Boat::class)->findOneBy([]);
+        $boat->setCoordX(0);
+        $boat->setCoordY(0);
+
+        // Remove any existing treasure from the map
+        $mapManager->removeTreasure();
+
+        // Place the treasure on a random island
+        $randomIsland = $mapManager->placeTreasureOnRandomIsland();
+        if ($randomIsland) {
+            $randomIsland->setHasTreasure(true);
+            $entityManager->persist($randomIsland);
+            $entityManager->flush();
+        }
+
+        // Redirect to the map page after starting the game
+        return $this->redirectToRoute('map');
     }
 }
